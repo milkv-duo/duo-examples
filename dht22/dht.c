8f75c46bbfcfd951618406d84bc04a6405f78c91
Paulo Antunes <81638459+pauloantuns@users.noreply.github.com>
2024-01-16 20:45:06 -0300

    Update and rename dht22.c to dht.c to support DHT11
    Modifications on read_dht22_dat() function to add support to DHT11 sensor.
    Defined DHTTYPE on line 11, can be 11 or 22.

--- dht22/dht.c
+++ dht22/dht.c
@@ -1,4 +1,3 @@
-
 // Ref: https://github.com/technion/lol_dht22/blob/master/dht22.c
 
 #include <stdio.h>
@@ -9,24 +8,23 @@
 #include <wiringx.h>
 
 #define MAXTIMINGS 85
-static int DHTPIN = 15;
+#define DHTTYPE 22
+
+static int DHTPIN = 16;
 static int dht22_dat[5] = {0, 0, 0, 0, 0};
 
-static uint8_t sizecvt(const int read)
-{
+static uint8_t sizecvt(const int read) {
     /* digitalRead() and friends from wiringpi are defined as returning a value
     < 256. However, they are returned as int() types. This is a safety function */
 
-    if (read > 255 || read < 0)
-    {
+    if (read > 255 || read < 0) {
         printf("Invalid data from wiringPi library\n");
         exit(EXIT_FAILURE);
     }
     return (uint8_t)read;
 }
 
-static int read_dht22_dat()
-{
+static int read_dht22_dat() {
     uint8_t laststate = HIGH;
     uint8_t counter = 0;
     uint8_t j = 0, i;
@@ -45,15 +43,12 @@ static int read_dht22_dat()
     pinMode(DHTPIN, PINMODE_INPUT);
 
     // detect change and read data
-    for (i = 0; i < MAXTIMINGS; i++)
-    {
+    for (i = 0; i < MAXTIMINGS; i++) {
         counter = 0;
-        while (sizecvt(digitalRead(DHTPIN)) == laststate)
-        {
+        while (sizecvt(digitalRead(DHTPIN)) == laststate) {
             counter++;
             delayMicroseconds(2);
-            if (counter == 255)
-            {
+            if (counter == 255) {
                 break;
             }
         }
@@ -63,8 +58,7 @@ static int read_dht22_dat()
             break;
 
         // ignore first 3 transitions
-        if ((i >= 4) && (i % 2 == 0))
-        {
+        if ((i >= 4) && (i % 2 == 0)) {
             // shove each bit into the storage bytes
             dht22_dat[j / 8] <<= 1;
             if (counter > 16)
@@ -75,44 +69,54 @@ static int read_dht22_dat()
 
     // check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
     // print it out if data is good
-    if ((j >= 40) &&
-        (dht22_dat[4] == ((dht22_dat[0] + dht22_dat[1] + dht22_dat[2] + dht22_dat[3]) & 0xFF)))
-    {
-        float t, h;
-        h = (float)dht22_dat[0] * 256 + (float)dht22_dat[1];
-        h /= 10;
-        t = (float)(dht22_dat[2] & 0x7F) * 256 + (float)dht22_dat[3];
-        t /= 10.0;
-        if ((dht22_dat[2] & 0x80) != 0)
-            t *= -1;
+    if(DHTTYPE == 11){
+        if ((j >= 40) && (dht22_dat[4] == ((dht22_dat[0] + dht22_dat[1] + dht22_dat[2] + dht22_dat[3]) & 0xFF))) {
+            float t, h;
+            h = (float)dht22_dat[0];
+            t = (float)dht22_dat[2];
+            if ((dht22_dat[3] & 0x80) != 0){
+                t = -(float)(dht22_dat[3] & 0x7F);
+            }
+            printf("Humidity = %.2f %% Temperature = %.2f *C \n", h, t);
+            return 1;
+        } else {
+            printf("Data not good, skip\n");
+            return 0;
+        }
+    }
+    else if (DHTTYPE == 22){
+        if ((j >= 40) && (dht22_dat[4] == ((dht22_dat[0] + dht22_dat[1] + dht22_dat[2] + dht22_dat[3]) & 0xFF))) {
+            float t, h;
+            h = (float)dht22_dat[0] * 256 + (float)dht22_dat[1];
+            h /= 10;
+            t = (float)(dht22_dat[2] & 0x7F) * 256 + (float)dht22_dat[3];
+            t /= 10.0;
+            if ((dht22_dat[2] & 0x80) != 0)
+                t *= -1;
 
-        printf("Humidity = %.2f %% Temperature = %.2f *C \n", h, t);
-        return 1;
-    }
-    else
-    {
-        printf("Data not good, skip\n");
-        return 0;
-    }
+            printf("Humidity = %.2f %% Temperature = %.2f *C \n", h, t);
+            return 1;
+        } else {
+            printf("Data not good, skip\n");
+            return 0;
+        }
+    } else{
+	printf("Wrong DHT type/model. Change in line 11.\n");
+	}
 }
 
-int main()
-{
-    if (wiringXSetup("duo", NULL) == -1)
-    {
+int main() {
+    if (wiringXSetup("duo", NULL) == -1) {
         wiringXGC();
         return -1;
     }
 
-    if (wiringXValidGPIO(DHTPIN) != 0)
-    {
+    if (wiringXValidGPIO(DHTPIN) != 0) {
         printf("Invalid GPIO %d\n", DHTPIN);
     }
 
-    while (1)
-    {
+    while (1) {
         read_dht22_dat();
-
         delayMicroseconds(1500000);
     }
 
