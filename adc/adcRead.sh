beac7b2e9ccd34f9a952c2df9d24007f2ce066b5
Paulo Antunes <81638459+pauloantuns@users.noreply.github.com>
2024-01-17 23:51:41 -0300

    Update adcRead.sh

--- adc/adcRead.sh
+++ adc/adcRead.sh
@@ -3,7 +3,7 @@
 saradc_path=$(find / -name "cv180x_saradc.ko" 2>/dev/null)
 
 if lsmod | grep -q "cv180x_saradc";then
-        echo "SARADC module already loaded.\n"
+        echo "SARADC module already loaded."
         echo "Define the ADC channel:
                 1: ADC1 (GP26|PIN31)
                 2: ADC2 (GP27|PIN32)
@@ -12,9 +12,32 @@ if lsmod | grep -q "cv180x_saradc";then
                 5: PWR_GPIO1
                 6: PWR_VBAT_"
         read adc_channel
+
         if [[ $adc_channel =~ ^[0-9]+$ ]]; then
-                echo "Channel $adc_channel"
+                echo "Enabling Channel $adc_channel"
+                echo $adc_channel > /sys/class/cvi-saradc/cvi-saradc0/device/cv_saradc
+                echo "ADC $adc_channel read: $(cat /sys/class/cvi-saradc/cvi-saradc0/device/cv_saradc)"
+        else
+                echo "Invalid ADC Channel."
+        fi
+
+else
+        insmod $saradc_path
+        echo "SARADC module loaded."
+        echo "Define the ADC channel:
+                1: ADC1 (GP26|PIN31)
+                2: ADC2 (GP27|PIN32)
+                3: ???
+                4: VDDC_RTC
+                5: PWR_GPIO1
+                6: PWR_VBAT_"
+        read adc_channel
+
+        if [[ $adc_channel =~ ^[0-9]+$ ]]; then
+                echo "Enabling Channel $adc_channel"
+                echo $adc_channel > /sys/class/cvi-saradc/cvi-saradc0/device/cv_saradc
+                echo "ADC $adc_channel read: $(cat /sys/class/cvi-saradc/cvi-saradc0/device/cv_saradc)"
+        else
+                echo "Invalid ADC Channel."
         fi
 fi
-
-insmod $saradc_path
